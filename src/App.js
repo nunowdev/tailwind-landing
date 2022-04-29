641493afa9565d841cf4545f4a5cb55fe127b8f3
Nuno <nunowdev@gmail.com>
2022-04-29 11:19:41 +0100

    Made Download Section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Download from "./components/Download";
 import Features from "./components/Features";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Features />
+      <Download />
     </div>
   );
 }
